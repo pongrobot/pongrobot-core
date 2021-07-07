6acc16b1dc7925d0ffb81056df159efe2de0680c
Andrew Gutierrez <agut@comcast.net>
2021-07-06 23:09:30 -0400

    Setup udev rules to create symlinks for multiple VESCs

--- utils/vesc_count.py
+++ utils/vesc_count.py
@@ -1,2 +1,21 @@
-# Output a count of the number of VESC devices already connected
-# Count any handles in /dev/ in the format /dev/vesc<n>
+import random
+import os
+import glob
+import serial
+import serial.tools.list_ports
+import sys
+
+portId = sys.argv[1]
+
+# Output an ID for a new vesc
+# Use pyserial to enumerate the ports and assign an ID
+serialPorts = serial.tools.list_ports.comports()
+vescs = []
+for port in serialPorts:
+	if port.description == "ChibiOS/RT Virtual COM Port":
+		vescs.append(port)
+
+vescs.sort(key=lambda x: x.name, reverse=False)
+for i, vesc in enumerate(vescs):
+	if vesc.name == portId:
+		print("vesc" + str(i))
